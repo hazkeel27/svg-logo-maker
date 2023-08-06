54879049ac69c8c845e416627384c26b75290807
hazkeel <hazkeel27@gmail.com>
2023-08-06 07:14:23 -0400

    all tests complete, application works

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -1,7 +1,7 @@
 const Shapes = require('../lib/shapes');
 
 describe('Shapes', () => {
-    describe('Wrong logoText length', () => {
+    describe('wrong logoText length', () => {
         it('throw error if logo text length > 3', ()=> {
             const shapes = () => new Shapes('SVGG', 'white', 'green');
             const error = new Error('Logo length must be 3 characters or less');
@@ -9,11 +9,27 @@ describe('Shapes', () => {
         });
     });
 
-    describe(' correct logoText length', () => {
+    describe('correct logoText length', () => {
         it('return logo text if length <= 3', ()=> {
             const shapes = new Shapes('SVG', 'white', 'green');
             const result = 'SVG'
             expect(shapes.logoText).toEqual(result);
+        });
+    });
+
+    describe('textColour', () => {
+        it('return hexadecimal text colour', ()=> {
+            const shapes = new Shapes('SVG', 'F0F0F0', '0F0F0F');
+            const result = '#F0F0F0'
+            expect(shapes.textColour).toEqual(result);
+        });
+    });
+
+    describe('logoColour', () => {
+        it('return hexadecimal logo colour', ()=> {
+            const shapes = new Shapes('SVG', 'F0F0F0', '0F0F0F');
+            const result = '#0F0F0F'
+            expect(shapes.logoColour).toEqual(result);
         });
     });
 
